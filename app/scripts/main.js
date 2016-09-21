5e83f00587b2331af8be2578b9e479f3002d3220
AndrewSC208 <meilingandrew@gmail.com>
2016-09-21 16:56:32 -0700

    Started text slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,35 @@
+$(document).ready(function() {
+	var slides = $('.text-slider').find('.slide');
+	var counter = initTextSlider(slides);
+
+	function initTextSlider(slides) {
+		$(slides[0]).addClass('display-slide');
+		return 0;
+	}
+
+	function nextSlide() {
+		$(slides[counter]).removeClass('display-slide');
+		counter++;
+		$(slides[counter]).addClass('display-slide');
+		console.log("From nextSlide: " + counter);
+	}
+
+	function prevSlide() {
+		$(slides[counter]).removeClass('display-slide');
+		counter--;
+		$(slides[counter]).addClass('display-slide');
+		console.log("From nextSlide: " + counter);	
+	}
+
+	$('.next').click(function() {
+		nextSlide();
+	});
+	
+	$('.prev').click(function() {
+		prevSlide();
+	});
+
+});
+
+
 
